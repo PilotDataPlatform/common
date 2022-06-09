036083701aae8d2bb3727f92e932d417e882fa86
zhiren <zzhan@indocresearch.org>
2022-06-09 09:07:30 -0400

    update the required version for dependencies

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ setuptools.setup(
     python_requires='>=3.6',
     install_requires=[
         'python-dotenv==0.19.1',
-        'httpx==0.22.0',
+        'httpx==0.23.0',
         'aioredis>=2.0.0<3.0.0.',
-        'aioboto3==^9.6.0',
-        'xmltodict==^0.13.0',
-        'minio==^7.1.8'
+        'aioboto3==9.6.0',
+        'xmltodict==0.13.0',
+        'minio==7.1.8'
     ],
     include_package_data=True,
     package_data={
